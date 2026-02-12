753122786e2701d6480e096f3bef6cd26507b666
Trayz72 <shahtanishq72@gmail.com>
2026-02-12 15:31:39 +0530

    fix

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,27 +2,23 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
+    options.binary_location = "/usr/bin/chromium"
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(options=options)
 
     yield driver
     driver.quit()
 
 
 def test_login(driver):
-    print("Starting Selenium Demo")
-
     driver.get("http://127.0.0.1:5050")
 
     driver.find_element(By.NAME, "username").send_keys("admin")
@@ -30,6 +26,3 @@ def test_login(driver):
     driver.find_element(By.NAME, "password").send_keys(Keys.RETURN)
 
     assert "Login successful!" in driver.page_source
-
-    print("Test Passed!")
-    print("Demo Complete!")
